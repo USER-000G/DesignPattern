d77901f761200a6f301966d5b60a69b793e45e72
USER-000G <2409174822@qq.com>
2020-05-19 16:39:18 +0800

    添加简单工厂方法模式

---
+++ FactoryActory.java
@@ -0,0 +1,42 @@
+interface FactoryActory 
+{
+	<T extends Product> T factory(Class<T> c);
+}
+
+interface Product
+{
+	void method1();
+	void method2();
+}
+
+class ConcreteFactory implements FactoryActory
+{
+	<T extends Product> T factory(Class<T> c) {
+		Product product = null;
+		try{
+			product = (Product) Class.forName(c.getName()).newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return (T) product;
+	}
+}
+
+class ConcreteProduct implements Product
+{
+	void method1() {
+		
+	}
+	void method2() {
+		
+	}
+}
+
+class FactoryActoryDemo
+{
+	public static void main(String args[]) {
+		FactoryActory factoryActory = new ConcreteFactory();
+		Product product = factoryActory.factory(ConcreteProduct.class);
+		// ÒµÎñ´úÂë
+	}
+}
